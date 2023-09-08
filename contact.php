fdfec84207ad3a923bb5428f58d7df05dba5b766
Sethu Satheesh <118425907+WH1T3-E4GL3@users.noreply.github.com>
2023-09-08 14:23:04 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php require "includes/header.php"; ?>
+
+<section class="contact-section" id="home">
+  <div class="contact-container">
+    <div class="contact-content">
+      <img src="assets/images/logo.png" alt="ICON" width="70" height="70"> 
+      <br>
+      <h2>Address</h2>
+      <p class="contact-text">
+        Taaza Restaurant, Palarivattom, Pillar number 535<br>
+        Kochi, Kerala , India<br>
+        Pin 682025
+      </p>
+      <br>
+      <h2>Contact Details</h2>
+      <p class="contact-text">
+        MOBILE: +91 7558 9513 51 | TELEPHONE: 0484-292674<br>
+        EMAIL : taaza001@gmail.com<br>
+      </p>
+      <form action="" class="contact-form" method="POST">
+        <input type="email" name="email_address" aria-label="email" placeholder="Your Email Address..." required
+          class="email-field">
+        <button type="submit" class="btn">Get Response Back</button>
+      </form>
+    </div>
+    <figure class="hero-banner">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125751.03807260178!2d76.18464664299258!3d9.957243150760043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d5b14f1bc7d%3A0xfa43e7937b630001!2sAl%20Taza!5e0!3m2!1sen!2sin!4v1694160076193!5m2!1sen!2sin" width="700" height="600" style="border:1;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </figure>
+  </div>
+</section>
+
+<?php require "includes/footer.php"; ?>
