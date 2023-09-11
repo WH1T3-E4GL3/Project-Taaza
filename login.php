87a8c7a16b3b1298d43d2f86e2a89169355fb0c4
Sethu Satheesh <118425907+WH1T3-E4GL3@users.noreply.github.com>
2023-09-11 20:15:21 +0530

    Update login.php

--- login.php
+++ login.php
@@ -18,8 +18,8 @@ function sendMail($email, $verification_code) // Mail sending function starts
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'taaza0restaurant@gmail.com';                     //SMTP username
-    $mail->Password   = 'sgdatimhzgwhcljg';                               //SMTP password
+    $mail->Username   = 'yourgmail@gmail.com';                     //SMTP username
+    $mail->Password   = 'youtAppPassword';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
