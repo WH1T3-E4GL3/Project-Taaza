81dfb98008445c6b338f3df99ab2ab84756201ff
Sethu Satheesh <118425907+WH1T3-E4GL3@users.noreply.github.com>
2024-01-18 11:38:39 +0530

    Update admin-forgot-pass.php

--- admin/admin-forgot-pass.php
+++ admin/admin-forgot-pass.php
@@ -19,14 +19,14 @@ function sendMail($email,$reset_token)
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'Youremail@gmail.com';                     //SMTP username
-    $mail->Password   = 'YourPassword';                               //SMTP password
+    $mail->Username   = 'YourEmail@gmail.com';                     //SMTP username
+    $mail->Password   = 'YourAppPassword';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('YourEmail@gmail.com', 'Taaza Restaurant');
-    $mail->addAddress('YourEmail@gmail.com');     //Add a recipient
+    $mail->addAddress('RecipientEmail@gmail.com');     //Add a recipient
 
 
     //Content
